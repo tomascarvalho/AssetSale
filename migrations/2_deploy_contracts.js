188dc744947abcd9d6f6e023941e03fec51a457c
Tomás Carvalho <tomcarvalhoc12@gmail.com>
2018-11-02 12:27:17 +0000

    Contracts are now being deployed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,26 @@
 var AssetToken = artifacts.require("AssetCrowdsaleToken");
 var AssetCrowdsale = artifacts.require("AssetCrowdsale");
 
-module.exports = function(deployer) {
-    deployer.deploy(AssetToken, "AssetToken#1", "AST#1").then(function() {
-        return deployer.deploy(1541003400, 1541010600, 1, 0x3F9EBd1f916BdCE3F11fd62a103f540B388Be6B2, 1000000, , 1000000);
-     });
-
+module.exports = function(deployer, network, accounts)
+{
+    const startTime = Date.now()/1000|0 + 120;
+    const endTime = startTime + (3600 * 1 * 1); // *1 hour *1 days
+    const rate = 1;
+    const wallet = accounts[0];
+    return deployer
+        .then(() => {
+            return deployer.deploy(AssetToken, 'AssetToken', 'AST');
+        })
+        .then(() => {
+            return deployer.deploy(
+                AssetCrowdsale,
+                startTime,
+                endTime,
+                rate,
+                wallet,
+                1000000,
+                AssetToken.address,
+                1000000
+            );
+        });
 };
